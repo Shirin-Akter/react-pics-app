e0328defb55b4102592aca1b106b967e6ca74639
Shirin Akter <shirin21rui@gmail.com>
2019-01-29 08:42:25 +0600

    First master commit of pics app in react

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+// const App = () => {
+
+
+class App extends React.Component{ 
+    onSearchSubmit(term){
+        
+    }
+    render(){
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
+    
+};
+
+export default App;
